80d3a139e70c610e9e59e06bf38ca00121848ee9
Guilherme <guilhermeavellar25@gmail.com>
2024-05-27 15:47:12 -0300

    exemplos de como funcionam as operações

---
+++ cap2_EstruturaLexica/mecanica_das_operacoes.js
@@ -0,0 +1,14 @@
+
+
+// inconsistência em operações de decimais, caracteristica da linguagem
+console.log("\noperações com decimais não são sempre precisas, exemplo:");
+console.log("0.2 -0.1 = ", 0.2 - 0.1);
+console.log("0.3 -0.2 = ", 0.3 - 0.2);
+console.log("0.8 -0.7 = ", 0.8 - 0.7);
+
+
+// o JS segue uma ideia de limite, do calculo 1 da matemática acadêmica, por exemplo:
+
+console.log("\nLógica de matemática acadêmica, limites, por exemplo:");
+console.log("1 / 0 = ", 1 / 0);
+console.log("1 / infity = ", 1 / Infinity);
--- cap2_EstruturaLexica/operacoes_decimais.js
+++
@@ -1,7 +0,0 @@
-
-
-// inconsistência em operações de decimais, caracteristica da linguagem
-console.log("\noperações com decimais não são sempre precisas, exemplo:");
-console.log("0.2 -0.1 = ", 0.2 - 0.1);
-console.log("0.3 -0.2 = ", 0.3 - 0.2);
-console.log("0.8 -0.7 = ", 0.8 - 0.7);
